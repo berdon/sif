ed1639cd3257d10dcbc9e7c407950e40baaf05e4
Austin Hanson <berdon@gmail.com>
2021-05-20 11:30:57 -0500

    Updates

--- examples/xml_to_json/xml_to_json.ts
+++ examples/xml_to_json/xml_to_json.ts
@@ -4,22 +4,25 @@ import data from "./tokens.json";
 let root: any = {};
 const grammar = new Grammar();
 grammar
-    .for("ROOT", p => p.expect("ELE", ctx => {
-        return root = ctx.value
-    }))
+    .for("ROOT", p => p.expect("ELE", ctx => root = ctx.value))
+
     .for("ELE", p => p
         .expect("ELE_TAG", ctx => ctx.bag.node = ctx.value)
         .expect("ELE_TAG_TAIL").do(ctx => { ctx.bag.node.children = ctx.value.length > 0 ? ctx.value : undefined; return ctx.bag.node }))
+
     .for("ELE_TAG_TAIL", p => p
         .expect("GTHAN", "ELE[]", ctx => ctx.bag.children = ctx.value)
         .expect("END_START IDENTIFIER GTHAN", ctx => ctx.bag.children))
     .for("ELE_TAG_TAIL", p => p.expect("FSLASH GTHAN", ctx => []))
+
     .for("ELE_TAG", p => p
         .expect("LTHAN IDENTIFIER", ctx => ctx.bag.tag = ctx.value)
         .expect("PROPERTY[]", ctx => ({ tag: ctx.bag.tag, properties: ctx.value.length > 0 ? ctx.value : undefined, children: [] })))
+
     .for("PROPERTY", p => p
         .expect("IDENTIFIER", ctx => ctx.bag.name = ctx.value)
         .expect("EQUAL PROPERTY_VALUE", ctx => ({ [ctx.bag.name]: ctx.value })))
+
     .for("PROPERTY_VALUE", p => p.expect("STRING", ctx => ctx.value.substring(1, ctx.value.length - 1)))
     .for("PROPERTY_VALUE", p => p.expect("INTEGER", ctx => Number(ctx.value)))
     .for("PROPERTY_VALUE", p => p.expect("FLOAT", ctx => Number(ctx.value)))
